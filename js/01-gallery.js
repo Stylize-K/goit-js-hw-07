ab2b68177352cf0e0bdc8096631e93350f512f8e
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-05-04 22:37:33 +0300

    Create and render gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const list = document.querySelector(".gallery");
+
+const markup = galleryItems
+	.map(
+		({ preview, original, description }) =>
+			`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`,
+	)
+	.join("");
+
+list.insertAdjacentHTML("afterbegin", markup);
