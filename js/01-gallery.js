b3bdb68237ddf5abf80ba246a4651facd8a79e71
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-05-05 20:48:04 +0300

    Task 1 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -24,3 +24,39 @@ const markup = galleryItems
 	.join("");
 
 list.insertAdjacentHTML("afterbegin", markup);
+
+// Створення слухача кліка по IMG
+
+list.addEventListener("click", onClickHandler);
+
+function onClickHandler(event) {
+	event.preventDefault();
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+
+	// Об'єкт налаштувань для функції .create() з бібліотеки "basicLightbox". У ньому прописуємо додавання/видалення слухача клавіші Esc при відкритті/закритті модального вікна відповідно.
+
+	const options = {
+		onShow: instance => {
+			window.addEventListener("keydown", closeByEsc);
+		},
+		onClose: instance => {
+			window.removeEventListener("keydown", closeByEsc);
+		},
+	};
+
+	// Створюємо модальне вікно та відкриваємо його.
+
+	const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`, options);
+
+	instance.show();
+
+	// Функція закриття модального вікна клішею Esc
+
+	function closeByEsc({ code }) {
+		if (code === "Escape") {
+			instance.close();
+		}
+	}
+}
